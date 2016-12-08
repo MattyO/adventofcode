f0a7845961120bb44cfdddfe74efe57cce45ed6c
Matt ODonnell <odonnell004@gmail.com>
2016-12-08 12:43:43 -0500

    day5 part2

--- day5.py
+++ day5.py
@@ -2,15 +2,16 @@ import hashlib
 
 input = "abbhdwsy"
 #input = "abc"
-code = ""
+code = [ None for i in range(8)]
 
+numbers = [ str(i) for i in  range(10)]
 i = 0
 while True:
     md5_output = hashlib.md5(input + str(i)).hexdigest()
-    if md5_output.startswith("00000"):
-        code += md5_output[5]
+    if md5_output.startswith("00000") and md5_output[5] in numbers and int(md5_output[5]) < 8 and code[int(md5_output[5])] is None :
+        code[int(md5_output[5])] = md5_output[6]
         print(i)
-        if len(code) == 8:
+        if None not in code:
             break;
     i +=1
 print code
